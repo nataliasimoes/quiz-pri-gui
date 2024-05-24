c7bebf5e44a09242715e6d8ff05c5f4e6998842e
Natália Simões <natalia.simoes.dev@gmail.com>
2024-05-24 20:01:35 -0300

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex flex-col gap-2">
+    <TheLogo class="text-5xl font-bold" />
+    <p class="hover:text-primary-400 transition">Prontos para o quiz?</p>
+    <!-- <USelectMenu v-model="category" :options="categories" class="mt-8" /> -->
+    <UButton
+      label="Começar!"
+      icon="i-heroicons-play-circle"
+      variant="outline"
+      class="py-4 hover:scale-110"
+      to="classic"
+      block
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { categories } from "~/values";
+import { useQuestionStore } from "~/store/question";
+
+const questionStore = useQuestionStore();
+const { category } = storeToRefs(questionStore);
+</script>
---
+++ pages/score.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="flex flex-col gap-2">
+    <h1 class="text-xl">Fim do Quiz!</h1>
+    <h2 class="text-xl">
+      Independente do resultado, amamos vocês e esperamos que aceitem o nosso
+      convite!
+    </h2>
+
+    <UCard class="gap-2 flex justify-center">
+      <div class="flex text-center gap-16">
+        <div class="relative">
+          <UIcon
+            name="i-heroicons-check-circle-solid"
+            class="bg-green-400 w-14 h-14"
+          />
+          <p>Correct : {{ questionStore.correctCount }}</p>
+        </div>
+        <div class="relative">
+          <UIcon
+            name="i-heroicons-x-circle-solid"
+            class="bg-red-400 w-14 h-14"
+          />
+          <p>
+            Incorrect : {{ questionStore.count - questionStore.correctCount }}
+          </p>
+        </div>
+      </div>
+    </UCard>
+
+    <UDivider />
+    <div class="flex flex-col gap-2">
+      <UButton
+        label="Play Again"
+        icon="i-heroicons-arrow-path"
+        block
+        to="/classic"
+      />
+      <UButton
+        label="Back to menu"
+        icon="i-heroicons-home-solid"
+        block
+        to="/"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useQuestionStore } from "~/store/question";
+
+const questionStore = useQuestionStore();
+</script>
